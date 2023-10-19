9fa8a6db33c421391a8a450d13a6ee7dab3fb45c
needlessbruiser <115527661+needlessbruiser@users.noreply.github.com>
2023-10-19 15:20:34 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+// Any PHP code can be added here if needed
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Explore Digital Marketing with GrowthMount.com</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            background-color: #f2f2f2;
+        }
+
+        .container {
+            max-width: 1200px;
+            text-align: center;
+            padding: 20px;
+        }
+
+        h1 {
+            color: #0066cc;
+            margin-bottom: 20px;
+        }
+
+        p {
+            font-size: 18px;
+            color: #333;
+            text-align: center;
+            margin: 10px 0;
+            line-height: 1.8;
+        }
+
+        a {
+            color: #0066cc;
+            text-decoration: none;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+
+        button {
+            background-color: #0066cc;
+            color: #fff;
+            border: none;
+            padding: 10px 20px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #0052a3;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Explore Digital Marketing with GrowthMount</h1>
+        <p>GrowthMount.com is your go-to source for all things digital marketing. From SEO strategies to content marketing insights, we provide you with the tools and knowledge to grow your online presence and achieve your business goals.</p>
+        <p>For more insights and expert advice, visit <a href="https://growthmount.com/">GrowthMount</a>.</p>
+
+        <div>
+        </div>
+        <a href="https://www.facebook.com/ahsanghaffar23/" target="_blank"><button>Connect on Facebook</button></a>
+
+    </div>
+</body>
+
+</html>
+
+<?php
+// Any additional PHP code can be added here if needed
+?>
